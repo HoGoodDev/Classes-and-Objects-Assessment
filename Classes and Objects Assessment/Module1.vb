aafc70c0ce6f7b4fc5df2fd9ae4e1d96a35984eb
CMP_HoGoodwill <CMP_HoGoodwill@RM37-7>
2024-01-30 09:36:22 -0500

    Add project files.

---
+++ Classes and Objects Assessment/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module module1
+    Dim totalSandwich As New List(Of Sandwich)
+
+    Sub Main()
+
+        Dim sandwich1 As New Sandwich()
+        Dim sandwich2 As New Sandwich(“peanut butter and jelly”, “white”, 1.0)
+        Dim sandwich3 As New Sandwich()
+        Dim sandwich4 As New Sandwich("Roast Beef”, “Rye”, 15.0)
+
+        sandwich3.MainIngredients = "chicken"
+        sandwich3.Price = 6.99
+        totalSandwich.Add(sandwich1)
+        totalSandwich.Add(sandwich2)
+        totalSandwich.Add(sandwich3)
+        totalSandwich.Add(sandwich4)
+
+        For i As Integer = 0 To totalSandwich.Count - 1
+            Console.WriteLine(totalSandwich(i))
+        Next
+
+        'Dim obj1, obj2, obj3 As New Sandwich
+        'Dim obj1 As New 
+
+    End Sub
+
+End Module
---
+++ Classes and Objects Assessment/Sandwich.vb
@@ -0,0 +1,66 @@
+﻿Public Class Sandwich
+    Private _mainIngredients As String
+    Private _breadType As String
+    Private _price As Double
+
+    Public Property MainIngredients As String
+
+        Get
+            Return _mainIngredients
+        End Get
+        Set(value As String)
+            _mainIngredients = value
+        End Set
+
+    End Property
+
+    Public Property BreadType As String
+
+        Get
+            Return _breadType
+        End Get
+        Set(value As String)
+            _breadType = value
+        End Set
+    End Property
+
+    Public Property Price As Double
+        Get
+            Return _price
+        End Get
+        Set(value As Double)
+            If value < 0.5 OrElse value > 9.5 Then
+                _price = 10
+            Else
+                _price = value
+            End If
+
+        End Set
+
+    End Property
+
+    Public Sub New()
+
+        MainIngredients = "tuna"
+
+        BreadType = "wheat"
+
+        Price = "4.99"
+
+    End Sub
+
+    Public Sub New(breadtype As String, ingredient As String, price As Double)
+
+        Me.BreadType = breadtype
+        Me.Price = price
+        Me.MainIngredients = ingredient
+
+    End Sub
+
+    Public Overrides Function ToString() As String
+
+        Return $"A {MainIngredients} sandwich on {BreadType} bread costs {Price}$"
+
+    End Function
+
+End Class
